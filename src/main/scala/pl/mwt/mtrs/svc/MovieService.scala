0ef5c501ed58b0dcfc3749dec6f82374bdd29d51
Maciej <mwojterski@gmail.com>
2017-09-17 20:58:08 +0200

    Movie service def

--- src/main/scala/pl/mwt/mtrs/svc/MovieService.scala
+++ src/main/scala/pl/mwt/mtrs/svc/MovieService.scala
@@ -1,18 +1,26 @@
 package pl.mwt.mtrs.svc
 
-import pl.mwt.mtrs.data.{MovieDef, MovieId}
+import pl.mwt.mtrs.data.{MovieDef, MovieId, MovieInfo}
 
 import scala.concurrent.Future
 
-sealed trait Result
-object Accepted extends Result
-case class Rejected(msg: String) extends Result
+object Registration extends Enumeration {
+  val Accepted, InvalidImdbId, Exists = Value
+}
+
+object Reservation extends Enumeration {
+  val Accepted, NoMovie, NoSeats = Value
+}
+
+sealed trait Info
+object NoMovie extends Info
+final case class HasInfo(info: MovieInfo) extends Info
 
 trait MovieService {
 
-  def register(movieDef: MovieDef): Future[Result]
+  def register(movieDef: MovieDef): Future[Registration.Value]
 
-  def reserveSeat(movieId: MovieId): Future[Result]
+  def reserveSeat(movieId: MovieId): Future[Reservation.Value]
 
-  def getInformation(movieId: MovieId): Future[String]
+  def getInformation(movieId: MovieId): Future[Info]
 }
