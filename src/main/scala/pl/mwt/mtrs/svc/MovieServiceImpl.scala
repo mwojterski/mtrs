ef9fec25b5a99e394325883e4544461f50bce5ee
Maciej <mwojterski@gmail.com>
2017-09-17 21:01:22 +0200

    Movie service implementation

--- src/main/scala/pl/mwt/mtrs/svc/MovieServiceImpl.scala
+++ src/main/scala/pl/mwt/mtrs/svc/MovieServiceImpl.scala
@@ -1,20 +1,87 @@
 package pl.mwt.mtrs.svc
 
-import pl.mwt.mtrs.data.{MovieDef, MovieId}
+import com.typesafe.scalalogging.StrictLogging
+import pl.mwt.mtrs.data.{MovieDef, MovieId, MovieInfo}
+import pl.mwt.mtrs.imdb.MovieTitleService
+import pl.mwt.mtrs.storage.MovieStore
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.implicitConversions
 
-object MovieServiceImpl
-    extends MovieService {
+class MovieServiceImpl(movieStore  : MovieStore,
+                       titleService: MovieTitleService)
+                      (implicit ec: ExecutionContext)
+    extends MovieService
+       with StrictLogging {
 
-  override def register(movieDef: MovieDef): Future[Result] =
-    Future successful {
-      if (movieDef.imdbId == "none") Rejected(s"Sorry, movie $movieDef not found") else Accepted
-    }
+  override def register(movieDef: MovieDef) =
+    findTitle(movieDef.imdbId)
+      .flatMap {
+        case Some(title) =>
+          storeMovie(movieDef, title)
 
-  override def reserveSeat(movieId: MovieId): Future[Result] =
-    Future successful (if (movieId.imdbId == "nope") Rejected("sorry, no more seats") else Accepted)
+        case None =>
+          Registration.InvalidImdbId
+      }
 
-  override def getInformation(movieId: MovieId): Future[String] =
-    Future successful s"Awesome information about $movieId"
+  override def reserveSeat(movieId: MovieId) =
+    movieStore
+      .findMovie(movieId)
+      .flatMap {
+        case Some(movie) =>
+          checkAndTryReservingSeat(movie)
+
+        case None =>
+          Reservation.NoMovie
+      }
+
+  override def getInformation(movieId: MovieId) =
+    movieStore
+      .findMovie(movieId)
+      .map {
+        case Some(movie) =>
+          HasInfo(movie)
+
+        case None =>
+          NoMovie
+      }
+
+  private def findTitle(imdbId: String): Future[Option[String]] =
+    movieStore
+      .findTitle(imdbId)
+      .flatMap {
+        case None =>
+          logger info("Cannot find title for '{}' locally, trying external service", imdbId)
+          titleService getTitle imdbId
+
+        case title => title
+      }
+
+  private def storeMovie(movieDef: MovieDef, title: String) =
+    movieStore
+      .add(movieDef, title)
+      .map {
+        if (_) Registration.Accepted
+        else   Registration.Exists
+      }
+
+  private def checkAndTryReservingSeat(movie: MovieInfo): Future[Reservation.Value] =
+    if (movie.reservedSeats < movie.availableSeats)
+      tryReservingSeat(movie)
+
+    else Reservation.NoSeats
+
+  private def tryReservingSeat(movie: MovieInfo) =
+    movieStore
+      .compareAndSetReservedSeats(movie, movie.reservedSeats, movie.reservedSeats + 1)
+      .flatMap {
+        if (_) Reservation.Accepted
+        else {
+          logger info("Lost race, retrying reservation for imdbId='{}', screenId='{}'", movie.imdbId, movie.screenId)
+          reserveSeat(movie)
+        }
+      }
+
+  private implicit def immediateResult[A](a: A): Future[A] =
+    Future successful a
 }
